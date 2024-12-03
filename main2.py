20c05c15c919adf8d8fd5ab5d8687b3a91c1dc38
RUSSIANITSPECIALIST333 <isus2022@mail.ru>
2024-12-03 15:40:59 +0300

    Сделал

--- main2.py
+++ main2.py
@@ -20,13 +20,14 @@ class Slivik:
             self.last_element = new_Node
         self.size += 1
 
+    def recursive_str(self, node):
+        if node is None:
+            return ""
+        next_str = self.recursive_str(node.next)
+        return str(node.value) + " " + next_str
+
     def __str__(self):
-        current = self.first_element
-        result = []
-        while current:
-            result.append(current.value)
-            current = current.next
-        return str(result)
+        return "[" + self.recursive_str(self.first_element)[:-1] + "]"
 
     def get(self, index):
         if index < 0 or index >= self.size:
@@ -35,6 +36,29 @@ class Slivik:
         for _ in range(index):
             current = current.next
         return current.value
+
+    def __getitem__(self, index):
+        return self.get(index)
+
+    def __adding_elements__(self, other):
+        new_array = Slivik()
+        current1 = self.first_element
+        current2 = other.first_element
+        
+        while current1 and current2:
+            new_array.append(current1.value + current2.value)
+            current1 = current1.next
+            current2 = current2.next
+            
+        return new_array
+
+    def addition(self, index, value):
+        if index < 0 or index >= self.size:
+            raise IndexError("Index out of range")
+        new_Node = self.Node(value)
+        if index == 0:
+            new_Node.next = self.first_element
+            self.first_element = new_Node
 
     def remove(self, index):
         if index < 0 or index >= self.size:
@@ -57,7 +81,7 @@ class Slivik:
                     current.value, next_node.value = next_node.value, current.value
                 next_node = next_node.next
             current = current.next
-
+    
 myarray = Slivik()
 myarray.append(1)
 myarray.append(3)
@@ -68,3 +92,5 @@ myarray.remove(1)
 print(myarray)  # [1, 2]
 myarray.sort()
 print(myarray)  # [1, 2]
+myarray.__adding_elements__(myarray)
+print(myarray)
